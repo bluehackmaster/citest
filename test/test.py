4be3bff554c31a11cac23079981b574235b34ae4
Youngbok Yoon <master@bluehack.net>
2018-02-10 14:53:24 +0900

    Changed host

--- test/test.py
+++ test/test.py
@@ -1,11 +1,11 @@
 import requests
 
-    def test():
-        "GET request to url returns a 200"
-        # url = 'http://api-prod.stylelens.io/playgrounds/images/datasets/deepfashion/categories'
-        url = 'http://www.naver.com'
-        payload = {}
-        # payload = {'category': "Blouse", 'offset': 0, 'limit': 10}
-        resp = requests.get(url, params=payload)
-        # j = resp.json()
-        assert resp.status_code == 200
+def test():
+    "GET request to url returns a 200"
+    # url = 'http://api-prod.stylelens.io/playgrounds/images/datasets/deepfashion/categories'
+    url = 'http://www.naver.com'
+    payload = {}
+    # payload = {'category': "Blouse", 'offset': 0, 'limit': 10}
+    resp = requests.get(url, params=payload)
+    # j = resp.json()
+      assert resp.status_code == 200
