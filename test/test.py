5644a143845f1a0f2ab744360a8dfb000781fcda
Youngbok Yoon <master@bluehack.net>
2018-02-10 15:21:09 +0900

    Fail test

--- test/test.py
+++ test/test.py
@@ -3,9 +3,45 @@ import requests
 def test():
     "GET request to url returns a 200"
     url = 'http://api-prod.stylelens.io/playgrounds/images/datasets/deepfashion/categories'
-    # url = 'http://www.naver.com'
-    # payload = {}
     payload = {'category': "Blouse", 'offset': 0, 'limit': 10}
     resp = requests.get(url, params=payload)
-    # j = resp.json()
     assert resp.status_code == 200
+
+    j = resp.json()
+
+    print(j)
+    data = j.get('data')
+    assert data != None
+    images = data.get('images')
+    assert images != None
+
+
+def test2():
+    "GET request to url returns a 200"
+    url = 'http://api-prod.stylelens.io/playgrounds/images/datasets/deepfashion/categories'
+    payload = {'category': "Blouse", 'offset': 0, 'limit': 10}
+    resp = requests.get(url, params=payload)
+    assert resp.status_code == 200
+
+    j = resp.json()
+
+    print(j)
+    data = j.get('data')
+    assert data != None
+    images = data.get('images')
+    assert images != None
+
+def test3():
+    "GET request to url returns a 200"
+    url = 'http://api-prod.stylelens.io/playgrounds/images/datasets/deepfashion/categories'
+    payload = {'category': "Blouse", 'offset': 0, 'limit': 10}
+    resp = requests.get(url, params=payload)
+    assert resp.status_code == 300
+
+    j = resp.json()
+
+    print(j)
+    data = j.get('data')
+    assert data != None
+    images = data.get('images')
+    assert images != None
